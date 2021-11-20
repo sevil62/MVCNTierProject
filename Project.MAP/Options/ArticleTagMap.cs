a6de6c8180ffb7587c95dc2974247d0da5999698
sevil aydın <sevar2362@gmail.com>
2021-11-21 01:34:40 +0300

    MAP Katmanı DAL Katmanı tamamlandı ve BLL Katmanına başlangıç yapıldı

---
+++ Project.MAP/Options/ArticleTagMap.cs
@@ -0,0 +1,24 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+   public class ArticleTagMap:BaseMap<ArticleTag>
+    {
+        public ArticleTagMap()
+        {
+            Ignore(x => x.ID);
+            HasKey(x => new
+            {
+                x.TagID,
+                x.ArticleID
+
+
+            });
+        }
+    }
+}
---
+++ Prpject.DAL/Context/MyContext.cs
@@ -0,0 +1,40 @@
+﻿using Project.ENTITIES.Models;
+using Project.MAP.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prpject.DAL.Context
+{
+   public class MyContext:DbContext
+    {
+        public MyContext():base("myConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new ArticleMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new ArticleTagMap());
+            modelBuilder.Configurations.Add(new TagMap());
+            modelBuilder.Configurations.Add(new UserProfileMap());
+            modelBuilder.Configurations.Add(new EditorMap());
+
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<UserProfile> AppUserProfiles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<ArticleTag> ArticleTags { get; set; }
+        public DbSet<Editor> Editors { get; set; }
+
+    }
+}
